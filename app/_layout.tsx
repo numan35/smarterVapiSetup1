58576b9891c93ac62be3a861414c2e82242ffde4
numan35 <32787732+numan35@users.noreply.github.com>
2025-09-25 20:37:28 -0400

    Change initial route to index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,5 +4,5 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   // While testing, start directly on /test-brain so you don’t need to navigate.
   // Later, you can switch this to "jason-chat" or remove initialRouteName.
-  return <Stack initialRouteName="test-brain" />;
+  return <Stack initialRouteName="index" />;
 }
