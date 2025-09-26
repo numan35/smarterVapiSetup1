2cefa1f5c8aa981b8cb9e6fc10fc1ef1ba58eb09
numan35 <32787732+numan35@users.noreply.github.com>
2025-09-25 20:37:29 -0400

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 // app/_layout.tsx
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   // While testing, start directly on /test-brain so you don’t need to navigate.
   // Later, you can switch this to "jason-chat" or remove initialRouteName.
   return <Stack initialRouteName="index" />;
